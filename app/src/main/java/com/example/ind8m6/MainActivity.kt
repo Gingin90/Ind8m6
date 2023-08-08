dfc0b2637aa15fb68b97b43f034dd8d8b538c089
ginger cea <gingercea@gmail.com>
2023-08-07 22:49:49 -0400

    se crea remote local y data  mas la data class de raza

---
+++ app/src/main/java/com/example/ind8m6/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.ind8m6
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.ej5m6.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
